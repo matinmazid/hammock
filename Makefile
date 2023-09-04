72fee8b99ca6206df3e41362d3a919e2403069bd
matin <matin_mazid@hotmai.com>
2023-09-04 16:29:22 -0400

    creates left window

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 CFLAGS := -g
 SRCDIR := src/
 
-
 hammock: $(SRCDIR)gui.c
 	gcc $(CFLAGS) $(SRCDIR)gui.c -o hammock -L/usr/lib -lncurses
 	chmod u+x hammock
+
+run_new: hammock
+	./hammock
 
 clean:
 	rm -r hammock
--- src/gui.c
+++ src/gui.c
@@ -1,80 +1,70 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <stdlib.h>
+void destroy_win(WINDOW *local_win);
+
 /**************** STRUCTS *****************/
-struct point
+struct guiWindow
 {
 	int x;
 	int y;
+	WINDOW *widowRef;
 };
-
-FILE *openFile(char *fileName)
-{
-
-	FILE *ptr = fopen(fileName, "r");
-	if (ptr == NULL)
-	{
-		perror("cant open file");
-		exit(-1);
-	}
-
-	return ptr;
-}
 
 /**************** FUNCTIONS *****************/
 
-void myLoop(int max_x, int max_y, FILE *pfile)
+WINDOW *drawRightWindow(void)
 {
-	fprintf(stderr, "good bad world\n");
-	printw("kdkd %d %d\n", max_x, max_y);
-	int ch;
-	struct point myPoint;
-	myPoint.x = 0;
-	myPoint.y = 0;
 
-	int x, y, prev;
-	while ((ch = fgetc(pfile)) != EOF)
-	{
-		getyx(stdscr, y, x);
-		if (y == (max_x - 1))
-		{
-			printw("press any key ");
-			getch();
-			move(0, 0);
-		}
+	WINDOW *windowsPtr = newwin(LINES, COLS / 2, 0, 0);
+	printw("hell world %d %d", (COLS / 2), (COLS / 2));
+	box(windowsPtr, 0, 0);
+	wrefresh(windowsPtr);
 
-		if (prev == '/' && ch == '*')
-		{
-			attron(COLOR_GREEN);
-			getyx(stdscr, y, x);
-			move(y, x - 1);
-			printw("%c%c", '/', ch);
-		}
-		else
-			printw("%c", ch);
-		refresh();
-		if (prev == '*' && ch == '/')
-			attroff(COLOR_GREEN);
-		prev=ch;
-	}
+	return windowsPtr;
 }
 
+void destroy_win(WINDOW *local_win)
+{
+	/* box(local_win, ' ', ' '); : This won't produce the desired
+	 * result of erasing the window. It will leave it's four corners 
+	 * and so an ugly remnant of window. 
+	 */
+	wborder(local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+	/* The parameters taken are 
+	 * 1. win: the window on which to operate
+	 * 2. ls: character to be used for the left side of the window 
+	 * 3. rs: character to be used for the right side of the window 
+	 * 4. ts: character to be used for the top side of the window 
+	 * 5. bs: character to be used for the bottom side of the window 
+	 * 6. tl: character to be used for the top left corner of the window 
+	 * 7. tr: character to be used for the top right corner of the window 
+	 * 8. bl: character to be used for the bottom left corner of the window 
+	 * 9. br: character to be used for the bottom right corner of the window
+	 */
+	wrefresh(local_win);
+	delwin(local_win);
+}
 /****************************** MAIN **********************/
 int main()
 {
+	int startx, starty, width, height;
+	int ch;
+	printf("hell world");
+	struct guiWindow inputBox;
+	struct guiWindow outputBox;
+	initscr();			  /* Start curses mode 		*/
+	cbreak();			  /* Line buffering disabled, Pass on
+					 * everty thing to me 		*/
+	keypad(stdscr, TRUE); /* I need that nifty F1 	*/
+	refresh();
+	inputBox.widowRef = drawRightWindow();
+	while ((ch = getch()) != KEY_F(1))
+	{
+		inputBox.widowRef = drawRightWindow();
+	}
 
-init_color(20,20,20,20);
-
-	FILE *ptr = openFile("src/gui.c");
-	initscr(); /* Start curses mode			  */
-	raw();
-	keypad(stdscr, TRUE);
-	noecho();
-	printw("Type any character to see it in bold\n"); /* Print Hello World		  */
-	int maxX, maxY;
-	getmaxyx(stdscr, maxX, maxY);
-	myLoop(maxX, maxY, ptr);
 	endwin(); /* End curses mode		  */
-
 	return 0;
 }
+
